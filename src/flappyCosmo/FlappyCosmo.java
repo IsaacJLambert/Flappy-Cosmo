440b787ee4e4f7a2426919e58fb56ada833e416b
Isaac Lambert <65886303+IsaacJLambert@users.noreply.github.com>
2021-06-17 12:47:48 -0600

    Pipes move and are recreated randomly

--- src/flappyCosmo/FlappyCosmo.java
+++ src/flappyCosmo/FlappyCosmo.java
@@ -37,7 +37,7 @@ public class FlappyCosmo implements ActionListener {
         jframe.setTitle("Flappy Cosmo");
         jframe.setSize(WIDTH, HEIGHT);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //jframe.setResizable(false);
+        jframe.setResizable(false);
         jframe.setVisible(true);
 
         columns = new ArrayList<>();
@@ -49,8 +49,6 @@ public class FlappyCosmo implements ActionListener {
         addColumn(true);
 
         timer.start();
-
-        System.out.println("Hello");
 
     }
 
@@ -65,14 +63,12 @@ public class FlappyCosmo implements ActionListener {
         int height = 50 + random.nextInt(300);
 
         if (start) {
-
-            columns.add(new Rectangle(WIDTH + width + columns.size() * 300, HEIGHT + height - 120, width, height));
+            columns.add(new Rectangle(WIDTH + width + columns.size() * 300, HEIGHT - height - 120, width, height));
             columns.add(new Rectangle(WIDTH + width + (columns.size() - 1) * 300, 0, width, HEIGHT - height - space));
         }
         else {
-            columns.add(new Rectangle(columns.get(columns.size() - 1).x + 600, HEIGHT + height - 120, width, height));
+            columns.add(new Rectangle(columns.get(columns.size() - 1).x + 600, HEIGHT - height - 120, width, height));
             columns.add(new Rectangle(columns.get(columns.size() - 1).x, 0, width, HEIGHT - height - space));
-
         }
 
 
@@ -92,8 +88,8 @@ public class FlappyCosmo implements ActionListener {
         g.setColor(Color.red);
         g.fillRect(cosmo.x, cosmo.y, cosmo.width, cosmo.height);
 
-        for(Rectangle r : columns) {
-            paintColumn(g, r);
+        for(Rectangle column : columns) {
+            paintColumn(g, column);
         }
     }
 
@@ -106,9 +102,33 @@ public class FlappyCosmo implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         ticks++;
 
+        int speed = 10;
+
+        //movement of the columns
+        for(int i = 0; i < columns.size(); i++) {
+            Rectangle column = columns.get(i);
+            column.x -= speed;
+        }
+
+        //time passing for the gravity
         if(ticks % 2 == 0 && yMotion <= 15) {
             yMotion += 2;
         }
+
+        //make columns disappear
+        for(int i = 0; i < columns.size(); i++) {
+            Rectangle column = columns.get(i);
+            
+            if(column.x + column.width < 0) {
+                columns.remove(column);
+              
+                if(column.y == 0) {
+                    addColumn(false);
+                }
+            }
+        }
+
+        //gravity of cosmo
         cosmo.y += yMotion;
 
         renderer.repaint();
