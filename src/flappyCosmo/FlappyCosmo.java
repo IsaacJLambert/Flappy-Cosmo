69f3fd8866a03595193290470ed7a001fe6a6193
Isaac Lambert <65886303+IsaacJLambert@users.noreply.github.com>
2021-06-17 09:58:19 -0600

    Initial commit

---
+++ src/flappyCosmo/FlappyCosmo.java
@@ -0,0 +1,15 @@
+package flappyCosmo;
+
+public class FlappyCosmo {
+
+    public static FlappyCosmo flappyCosmo;
+
+    public FlappyCosmo() {
+
+    }
+
+    public static void main(String[] args) {
+
+        flappyCosmo = new FlappyCosmo();
+    }
+}
